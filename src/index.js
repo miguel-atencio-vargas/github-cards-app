bdc879a6ac529e4f2b239a14ad19342c70ffe019
skyfall947 <atencio947@gmail.com>
2021-05-13 20:54:07 -0400

    init commit
    Until here we've used creteRef() to take data from the DOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const CardList = ({ profiles }) => {
+  return (
+    <div>
+      {profiles.map((profile, i) => <Card key={i}{...profile} />)}
+    </div>
+  )
+}
+
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className='gihub-profile'>
+        <img className='img' src={profile.avatar_url} alt='img' />
+        <div className='info'>
+          <div className='name'>{profile.name}</div>
+          <div className='company'>{profile.company}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.userNameInput = React.createRef();
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(this.userNameInput.current.value)
+    }
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          type='text'
+          placeholder='Github Username'
+          ref={this.userNameInput}
+          required 
+        />
+        <button>Add Card</button>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: testData
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div className='header'>{this.props.title}</div>
+        <Form />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App title='The Github Cards App' />, document.getElementById('root'))
